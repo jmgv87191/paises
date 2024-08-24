35d47c9585a256befba1b28dcba4d065b17c77fc
jmgv87191 <jmgv87191@gmail.com>
2024-08-24 08:53:46 -0700

    as

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-page',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor( private aRoute:ActivatedRoute ){}
+
+  ngOnInit(): void {
+
+    
+
+  }
 
 }
