6825ee0dacf2fb57e0d06e55dfb8d4a722386dd6
jmgv87191 <jmgv87191@gmail.com>
2024-08-24 11:19:57 -0700

    asd

--- src/app/countries/components/country-table/country-table.component.ts
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-country-table',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './country-table.component.html',
   styleUrl: './country-table.component.css'
 })
--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
 import { Country } from '../../interfaces/country';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-country-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
@@ -31,7 +32,6 @@ export class CountryPageComponent implements OnInit {
       }
 
       return this.country = country;
-
     } )
 
   }
