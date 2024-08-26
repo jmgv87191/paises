ffcb0bbf0d6aca8a109c08e171d7dca2d24a49cc
juan Manuel <jmgv87191@gmail.com>
2024-08-26 12:33:53 -0700

    asd

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,23 +3,27 @@ import { SearchBoxComponent } from '../../../shared/components/search-box/search
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 import { CountryTableComponent } from '../../components/country-table/country-table.component';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-by-capital-page',
   standalone: true,
-  imports: [SearchBoxComponent, CountryTableComponent],
+  imports: [SearchBoxComponent, CountryTableComponent, LoadingSpinnerComponent],
   templateUrl: './by-capital-page.component.html',
   styleUrl: './by-capital-page.component.css'
 })
 export class ByCapitalPageComponent {
 
   countries: Country[] = []
+  public isLoading: boolean = false;
 
   searchByCapital( term: string ){
 
+    this.isLoading = true
+
     this._countriesService.searchCapital( term ).subscribe((data)=>{
       this.countries = data
-
+      this.isLoading = false
     }) 
 
   }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
@@ -7,15 +8,38 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Input() placeholder: string = '';
   @Output() onValue = new EventEmitter<string>();
+  @Output() onDebounce = new EventEmitter<string>();
 
 
+  ngOnInit(): void {
+
+    this.debouncer.pipe(
+      debounceTime(300)
+    ).
+    
+    subscribe( value =>{
+
+      this.onDebounce.emit(value)
+
+    } )
+
+  }
   
   emitValue(  value: string ){
     this.onValue.emit( value )
+  }
+
+  onKeyPress( searchTerm: string ){
+
+    this.debouncer.next(searchTerm)
+
   }
 
 }
