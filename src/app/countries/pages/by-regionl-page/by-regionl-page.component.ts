b3a8d9f5b6d272914087f34fb8b6da7b189db7c1
juan Manuel <jmgv87191@gmail.com>
2024-08-26 14:15:34 -0700

    asd

--- src/app/countries/pages/by-regionl-page/by-regionl-page.component.ts
+++ src/app/countries/pages/by-regionl-page/by-regionl-page.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { SearchBoxComponent } from "../../../shared/components/search-box/search-box.component";
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 import { CountryTableComponent } from "../../components/country-table/country-table.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-by-regionl-page',
   standalone: true,
-  imports: [SearchBoxComponent, CountryTableComponent],
+  imports: [SearchBoxComponent, CountryTableComponent,CommonModule],
   templateUrl: './by-regionl-page.component.html',
   styleUrl: './by-regionl-page.component.css'
 })
 export class ByRegionlPageComponent {
 
-  countries: Country[] = []
+  countries: Country[] = [];
+  public regions: string[] = [  'Africa','Americas','Asia','Europe','Oceania' ];
+  public selectedRegion?:string;
+
 
   constructor( private countryServices:CountriesService ){}
 
   searchByRegion( term:string ){
+
+    this.selectedRegion = term;
     
     this.countryServices.searchByRegion( term ).subscribe((data)=>{
       this.countries = data
